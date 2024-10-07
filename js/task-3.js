8507a878bf4dc09fe9339dbbf3886374bfeb4456
sergeisumskoi <oceanbreeze018@ukr.net>
2024-10-07 20:30:01 +0300

    homework-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,17 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-  const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-  return totalWidth;
+'use strict';
+
+function filterArray(numbers, value) {
+  let newArray = [];
+  for (const num of numbers) {
+    if (num > value) {
+      newArray.push(num);
+    }
+  }
+  return newArray;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
